84d9187a040357afbb452232bcb22927423a6256
lucspec <lucspec@noreply.github.com>
2025-11-16 23:19:14 -0700

    neaten UI; update log structure

--- apps/loglog/app.js
+++ apps/loglog/app.js
@@ -99,41 +99,25 @@ function startRecord(force) {
   var layout = new Layout({ type: "v", c: [
     { type: "h", c: [
       { type: "v", c: [
-        {type:"txt", font:"6x8", label:"Samples", pad:2},
-        {type:"txt", id:"samples", font:"6x8:2", label:"  -  ", pad:5, bgCol:g.theme.bg},
+        {type:"txt", font:"6x8", label:"Samp", pad:1},
+        {type:"txt", id:"samples", font:"6x8:2", label:"-", pad:2, bgCol:g.theme.bg},
       ]},
       { type: "v", c: [
-        {type:"txt", font:"6x8", label:"Time", pad:2},
-        {type:"txt", id:"time", font:"6x8:2", label:"  -  ", pad:5, bgCol:g.theme.bg},
+        {type:"txt", font:"6x8", label:"Time", pad:1},
+        {type:"txt", id:"time", font:"6x8:2", label:"-", pad:2, bgCol:g.theme.bg},
       ]},
     ]},
     { type: "h", c: [
       { type: "v", c: [
-        {type:"txt", font:"6x8", label:"Max X", pad:2},
-        {type:"txt", id:"maxX", font:"6x8", label:"  -  ", pad:5, bgCol:g.theme.bg},
+        {type:"txt", font:"6x8", label:"MaxG", pad:1},
+        {type:"txt", id:"maxMag", font:"6x8:2", label:"-", pad:2, bgCol:g.theme.bg},
       ]},
       { type: "v", c: [
-        {type:"txt", font:"6x8", label:"Max Y", pad:2},
-        {type:"txt", id:"maxY", font:"6x8", label:"  -  ", pad:5, bgCol:g.theme.bg},
-      ]},
-      { type: "v", c: [
-        {type:"txt", font:"6x8", label:"Max Z", pad:2},
-        {type:"txt", id:"maxZ", font:"6x8", label:"  -  ", pad:5, bgCol:g.theme.bg},
+        {type:"txt", font:"6x8", label:"BPM", pad:1},
+        {type:"txt", id:"bpm", font:"6x8:2", label:"-", pad:2, bgCol:g.theme.bg},
       ]},
     ]},
-    {type:"txt", font:"6x8", label:"Max G", pad:2},
-    {type:"txt", id:"maxMag", font:"6x8:4", label:"  -  ", pad:5, bgCol:g.theme.bg},
-    { type: "h", c: [
-      { type: "v", c: [
-        {type:"txt", font:"6x8", label:"BPM", pad:2},
-        {type:"txt", id:"bpm", font:"6x8:2", label:"  -  ", pad:5, bgCol:g.theme.bg},
-      ]},
-      { type: "v", c: [
-        {type:"txt", font:"6x8", label:"Conf", pad:2},
-        {type:"txt", id:"conf", font:"6x8:2", label:"  -  ", pad:5, bgCol:g.theme.bg},
-      ]},
-    ]},
-    {type:"txt", id:"state", font:"6x8:2", label:"RECORDING", bgCol:"#f00", pad:5, fillx:1},
+    {type:"txt", id:"state", font:"6x8:2", label:"REC", bgCol:"#f00", pad:3, fillx:1},
   ]},
   {
     btns:[
@@ -145,7 +129,7 @@ function startRecord(force) {
         Bangle.removeListener('accel', accelHandler);
         Bangle.removeListener('HRM', hrmHandler);
         Bangle.setHRMPower(0);
-        layout.state.label = "STOPPED";
+        layout.state.label = "STOP";
         layout.state.bgCol = "#0f0";
         stopped = true;
         layout.render();
@@ -156,85 +140,63 @@ function startRecord(force) {
 
   // now start writing
   var f = require("Storage").open(getFileName(fileNumber), "w");
-  f.write("Time (ms),Battery,Source,X,Y,Z,Total,BPM,Confidence\n");
-  var start = getTime();
+  f.write("Epoch (ms),Battery,X,Y,Z,Total,BPM,Confidence\n");
+  var startTime = Date.now();
   var sampleCount = 0;
   var maxMag = 0;
-  var maxX = 0;
-  var maxY = 0;
-  var maxZ = 0;
-  var lastHRM = {bpm: 0, confidence: 0};
 
   function accelHandler(accel) {
-    var t = getTime()-start;
+    var t = Date.now();
     var battery = E.getBattery();
     
     if (logRawData) {
       f.write([
-        t*1000,
+        t,
         battery,
-        "accel",
         accel.x*8192,
         accel.y*8192,
         accel.z*8192,
         accel.mag*8192,
-        lastHRM.bpm,
-        lastHRM.confidence
+        "",
+        ""
       ].map(n=>Math.round(n)).join(",")+"\n");
     } else {
       f.write([
-        Math.round(t*1000),
+        t,
         battery,
-        "accel",
         accel.x,
         accel.y,
         accel.z,
         accel.mag,
-        lastHRM.bpm,
-        lastHRM.confidence
+        "",
+        ""
       ].join(",")+"\n");
     }
     
     if (accel.mag > maxMag) {
       maxMag = accel.mag.toFixed(2);
     }
-    if (accel.x > maxX) {
-      maxX = accel.x.toFixed(2);
-    }
-    if (accel.y > maxY) {
-      maxY = accel.y.toFixed(2);
-    }
-    if (accel.z > maxZ) {
-      maxZ = accel.z.toFixed(2);
-    }
 
     sampleCount++;
     layout.samples.label = sampleCount;
-    layout.time.label = Math.round(t)+"s";
-    layout.maxX.label = maxX;
-    layout.maxY.label = maxY;
-    layout.maxZ.label = maxZ;
+    layout.time.label = Math.round((Date.now()-startTime)/1000)+"s";
     layout.maxMag.label = maxMag;
     layout.render();
   }
 
   function hrmHandler(hrm) {
-    var t = getTime()-start;
+    var t = Date.now();
     var battery = E.getBattery();
     
-    lastHRM = {bpm: hrm.bpm, confidence: hrm.confidence};
-    
     f.write([
-      Math.round(t*1000),
+      t,
       battery,
-      "hrm",
-      "0","0","0","0",
+      "","","","",
       hrm.bpm,
       hrm.confidence
     ].join(",")+"\n");
     
     layout.bpm.label = hrm.bpm;
-    layout.conf.label = hrm.confidence;
     layout.render();
   }
 
